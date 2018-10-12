d6507365b081da56420b2cf33befe1becd19ea29
scruzo710 <sergiosco.93@gmail.com>
2018-10-11 19:00:47 -0600

    some changes on basic layout with flexbox and some minus styling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
     new WOW().init();
+
+   
+   
+
+        var btn = $('#topBtn');
+        // btn.hide();
+
+        $(window).scroll(function() {
+          if ($(window).scrollTop() > 300) {
+           
+            btn.addClass('show');
+          } else {
+            btn.removeClass('show');
+          }
+        });
+      
+        btn.on('click', function(e) {
+          e.preventDefault();
+          $('html, body').animate({scrollTop:0}, '300');
+        });
+      
+       
+
+     
+      
 });
